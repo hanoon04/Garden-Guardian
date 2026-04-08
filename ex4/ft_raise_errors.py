aa07d173d3cfa2f6afd08314a8735cf0970a2ed3
haneen tawalbeh <haneentawalbeh32@gmail.com>
2026-04-08 19:58:10 +0300

    jj

--- ex4/ft_raise_errors.py
+++ ex4/ft_raise_errors.py
@@ -6,7 +6,7 @@ def check_plant_health(name: str, water_level: int, sun_hours: int) -> None:
             raise ValueError("Plant name cannot be empty!")
         if water_level > 10:
             raise ValueError(f"Water level {water_level} is too high (max 10)")
-        if wter_level < 1:
+        if water_level < 1:
             raise ValueError(f"Water level {water_level} is too low (min 1)")
         if sun_hours < 2:
             raise ValueError(f"Sunlight hours {sun_hours} is too low (min 2)")
@@ -25,7 +25,7 @@ def test_plant_checks() -> None:
     check_plant_health("tomato", 5, 10)
     print()
     print("Testing empty plant name...")
-    check_plant_health(None, 5, 10)
+    check_plant_health("", 5, 10)
     print()
     print("Testing bad water level...")
     check_plant_health("tomato", 15, 10)
